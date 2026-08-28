8e9dfec5ea0ca8842766654322f35fda607ca561
EVAVO-STUDIO <evavo.studio@gmail.com>
2026-08-29 09:50:54 +1000

    refactor: stop mutating global RNG

--- scripts/run_seed_director.gd
+++ scripts/run_seed_director.gd
@@ -5,6 +5,7 @@ const RunSeedRules = preload("res://scripts/run_seed_rules.gd")
 var _scene_id := 0
 var _last_phase := -1
 var _last_mission_index := -1
+var current_mission_seed := RunSeedRules.mission_seed(0)
 
 func _process(_delta: float) -> void:
 	var scene := get_tree().current_scene
@@ -15,15 +16,15 @@ func _process(_delta: float) -> void:
 		_scene_id = scene_id
 		_last_phase = int(scene.get("phase"))
 		_last_mission_index = int(scene.get("mission_index"))
-		_seed_for_scene(scene)
+		_update_seed_for_scene(scene)
 		return
 
 	var phase := int(scene.get("phase"))
 	var mission_index := int(scene.get("mission_index"))
 	if phase != _last_phase and phase == 1:
-		_seed_for_scene(scene)
+		_update_seed_for_scene(scene)
 	elif mission_index != _last_mission_index and phase != 1:
-		_seed_for_scene(scene)
+		_update_seed_for_scene(scene)
 	_last_phase = phase
 	_last_mission_index = mission_index
 
@@ -36,12 +37,11 @@ func _supports(scene: Object) -> bool:
 			return false
 	return true
 
-func _seed_for_scene(scene: Object) -> void:
+func _update_seed_for_scene(scene: Object) -> void:
 	var mission_index := maxi(0, int(scene.get("mission_index")))
-	var run_seed := RunSeedRules.mission_seed(mission_index)
-	seed(run_seed)
+	current_mission_seed = RunSeedRules.mission_seed(mission_index)
 	if _has_property(scene, "status_text") and int(scene.get("phase")) != 1:
-		scene.set("status_text", "MISSION SEED %d" % run_seed)
+		scene.set("status_text", "MISSION SEED %d" % current_mission_seed)
 	if _has_property(scene, "status_timer") and int(scene.get("phase")) != 1:
 		scene.set("status_timer", 1.2)
 
