3565cb4f2e95465b93bc83f168310b6e4fd20ccc
EVAVO-STUDIO <evavo.studio@gmail.com>
2026-08-29 12:46:19 +1000

    fix: split pixel UI draw surface into parser-safe script

---
+++ scripts/pixel_ui_surface.gd
@@ -0,0 +1,8 @@
+class_name PixelUiSurface
+extends Control
+
+var director: Node
+
+func _draw() -> void:
+	if director != null and director.has_method("_draw_surface"):
+		director.call("_draw_surface", self)
